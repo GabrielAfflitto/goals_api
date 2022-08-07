d3029b982c142dde506dbeb73fbb0d7ef1ea42b6
GabrielAfflitto <gafflitto92@gmail.com>
2022-08-07 16:48:48 -0600

    created goals table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_06_211825) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_07_220919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_06_211825) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "goals", "users"
 end
