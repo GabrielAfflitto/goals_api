83bed45b27fc00d87d9b5f062d9fbbf87fdf7270
GabrielAfflitto <gafflitto92@gmail.com>
2022-08-06 19:57:00 -0600

    cleaned up syntax error in Authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token = jwt.encode(user_id: @user.id)
+      token = jwt_encode(user_id: @user.id)
       render json: {token: token}, status: :ok
     else
       render json: {error: 'Unauthorized'}, status: :unauthorized
